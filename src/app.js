3d2b5352bf7e4b0f1ac7c9ff3ffa820d1dc7c742
shaghayegh fathi <shaghayegh.fathi24000@gmail.com>
2021-06-20 12:19:50 +0430

    added cors and health route

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const cors = require("cors");
+const bodyParser = require("body-parser");
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const managerRouter = require('./routers/manager')
 const restaurantRouter = require('./routers/restaurant')
+const healthRouter = require('./routers/health')
 
 const app = express()
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.json())
 app.use(userRouter)
 app.use(managerRouter)
 app.use(restaurantRouter)
+app.use(healthRouter)
 
 module.exports = app
